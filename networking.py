fcdb7773d4f8f3a72edc88a5f788e3dcb53451e1
student <orikeidar01@gmail.com>
2018-10-16 10:42:00 +0300

    add Data class to represent data sent from server

--- networking.py
+++ networking.py
@@ -1,14 +1,22 @@
-import socket
-
+import socket,Queue,threading
+class Data(object):
+    
+    def __init__(self,param):
+        #TODO: implement Data fields and functions according to the protocall
+        pass
+    def __repr__(self):
+        pass
+    
 class Networking(object):
 
     def __init___(self,addr):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.addr = addr
+        self.data_queue = Queue()
+
     def connect(self):
         self.socket.connect(self.addr)
     def recv(self,size):
         self.data = self.socket.recv(size)
-    def __repr__(self):
-        pass
+
         

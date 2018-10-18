3317429cc0194470e8962a2f7207c8c4a0896282
student <orikeidar01@gmail.com>
2018-10-18 08:19:51 +0200

    add handling of connection and close + excpetion handling

--- networking.py
+++ networking.py
@@ -14,21 +14,53 @@ class Networking(object):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.addr = addr
         self.data_queue = Queue.Queue()
-        self.listen = False
         self.recv_size = 1024
         self.listen_thread = threading.Thread(target = lambda: self.listen_loop(self.recv_size))
+        self.send_thread = threading.Thread(target = self.send_loop)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+        self.data_buffer = None
+        self.connected = False
         
     def connect(self):
-        self.socket.connect(self.addr)
+        try:
+            self.socket.connect(self.addr)
+            self.connected = True
+        except socket.error as err:
+            print "canno't connect - socket error: " + str(err)
+    
+    def send_loop(self)
+        while connected:
+            if self.data_buffer:
+                try:
+                    self.send_data(self.data_buffer)
+                except socket.error as err:
+                    print "socket error: " + err
+                    self.close()
+                except Exception as err:
+                    print "general error in send_loop: " + str(err)
+                    raise err
+                finally:
+                    self.data_buffer = None
+                
+    def close(self):
+        self.connected = False
+        self.socket.close()
+        
+    def send_data(self,data):
+        self.data_buffer = data
         
     def recv(self,size):
         self.data = self.socket.recv(size)
         #TODO: add logic based on protocol
         
     def listen_loop(self,size):
-        while self.listen:
-            data = self.socket.recv(size)
-            self.data_queue.put(Data(data))
+        while self.connected:
+            try:
+                data = self.recv(size)
+                self.data_queue.put(Data(data))
+            except socket.error as err:
+                print "socket error: " + str(err)
+                self.close()
             
     def start_listening(self):
         self.listen = True

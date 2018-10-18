0d8f2fbd358f81eb87b0d09d891a7c68fb53f022
dror1212 <drortal12@gmail.com>
2018-10-18 11:35:50 +0300

    add better solution

--- bruteForce.py
+++ bruteForce.py
@@ -1,5 +1,4 @@
 import md5
-import threading
 
 class MD5():
     def __init__(self,start,stop,md):
@@ -8,7 +7,6 @@ class MD5():
         self.mdStr=md
         self.l=len(start)
         self.hlp=self.start
-        self.middle = self.seperate()
 
     def tryy(self,index,hlp):
         hlp = list(hlp)
@@ -19,24 +17,10 @@ class MD5():
         hlp = ''.join(hlp)
         return hlp
 
-    def seperate(self):
-        start2 = list(self.start)
-        stop2 = list(self.stop)
-        s = []
-        z=""
-        for x in range(0,len(start2)):
-            s.append(ord(stop2[x])-ord(start2[x]))
-        for x in range(0,len(s)):
-            z=z+chr(ord(start2[x])+(s[x]/2))
-        return z
-
-    def do():
-        pass #to do, threading
 #####recursive check#####
     def lastCheck(self):
         for x in range(ord("a"),ord("z")+1):
             ezer = md5.new(self.hlp).hexdigest()
-            print self.hlp
             if ezer==self.mdStr:
                 return self.hlp
             self.hlp = self.tryy(self.l-1,self.hlp)
@@ -49,54 +33,33 @@ class MD5():
             if z>0:
                 ezra = self.newCheck(z-1)
                 if ezra!="nope":
-                    if ezra!="not found":
-                        return ezra
-                    return "not found"
+                    return ezra
                 self.hlp = self.tryy(self.l-z-1,self.hlp)
             else:
                 ezra = self.lastCheck()
                 if ezra!="nope":
-                    if ezra!="not found":
-                        return ezra
-                    return "not found"
+                    return ezra
         return "nope"
 
 ###########################
 
-####not recursive check####
-    def check(self):
-        hlp = self.start
-        for x in range(ord("a"),ord("z")+1):
-            for x in range(ord("a"),ord("z")+1):
-                for x in range(ord("a"),ord("z")+1):
-                    for x in range(ord("a"),ord("z")+1):
-                        for x in range(ord("a"),ord("z")+1):
-                            for x in range(ord("a"),ord("z")+1):
-                                ezer = md5.new(hlp).hexdigest()
-                                if ezer==self.mdStr:
-                                    return hlp
-                                hlp = self.tryy(5,hlp)
-                            hlp = self.tryy(4,hlp)
-                        hlp = self.tryy(3,hlp)
-                    hlp = self.tryy(2,hlp)
-                hlp = self.tryy(1,hlp)
-            hlp = self.tryy(0,hlp)
-
+    def smartCheck(self):
+        while self.hlp!=self.stop:
+            ezer = md5.new(self.hlp).hexdigest()
+            if ezer==self.mdStr:
+                return self.hlp
+            hlp2 = list(self.hlp)
+            hlp2[len(hlp2)-1] = chr(ord(hlp2[len(hlp2)-1])+1)
+            for x in range(0,len(hlp2)):
+                if ord(hlp2[len(hlp2)-1-x])>ord("z"):
+                    hlp2[len(hlp2)-1-x]="a"
+                    hlp2[len(hlp2)-1-x-1]= chr(ord(hlp2[len(hlp2)-1-x-1])+1)
+            if ord(hlp2[0])>ord("z"):
+                return "not found"
+            self.hlp = ''.join(hlp2)
         return "not found"
-#############################
+                    
+                
+x = MD5("aaaaa","azzzz","9b7996ec2cfb97bd416dbb3a7367ad4e")
 
-        
-start = "aaaaa"
-stop = "bbbbb"
-
-x = MD5(start,stop,"de4b33a0ca6fcc673c62f3952a7daa21")
-
-x.do()
-
-
-
-            
-                            
-                            
-                            
-                                    
+print x.smartCheck()

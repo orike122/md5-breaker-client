f458eb3ddaad3b1bfcfdffb05be156045d81a3d7
student <orikeidar01@gmail.com>
2018-10-28 13:33:35 +0200

    Add demo

--- demo_admin.py
+++ demo_admin.py
@@ -2,6 +2,16 @@ import socket
 
 
 def main():
-    pass
+    soc = socket.scoket(socket.AF_INET, socket.SOCK_STREAM)
+    soc.bind(("0.0.0.0",4320))
+    soc.listen(1)
+    client,client_addr = soc.accept()
+    sendmd5(client)
+    while True:
+        print client.recv(1024)
+
+def sendmd5(soc):
+    soc.send("start:aaaaa,stop:aaaaz,md5:e80b5017098950fc58aad83c8c14978e")
+    
 if __name__ == "__main__":
     main()
